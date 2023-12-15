eb7e12560f355f82926e2473754903163540007d
Developer <developer@Developers-Mac-mini.local>
2023-12-15 21:29:54 +0530

    filter java8 ex

---
+++ src/main/java/com/java8/java8ex/Java8exApplication.java
@@ -0,0 +1,13 @@
+package com.java8.java8ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8exApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8exApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java8/java8ex/Java8exApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java8.java8ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8exApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
